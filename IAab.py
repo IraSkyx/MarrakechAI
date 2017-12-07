45b23445de501ef6f7007596782c8afe60e606b6
IraSkyx <planete.skyx@gmail.com>
2017-12-07 10:35:55 +0100

    Added pass instructions in function

--- IAab.py
+++ IAab.py
@@ -8,11 +8,16 @@ import random
 class IA (JoueurMarrakech):
 
     def __str__(self):
+        pass
 
     def __init__(self):
+        pass
 
     def changer_direction(self, modele):
+        pass
 
     def avancer(self, nb_cartes_deplacement, modele):
+        pass
 
     def ou_poser_tapis(self, coups_possibles, modele):
+        pass
